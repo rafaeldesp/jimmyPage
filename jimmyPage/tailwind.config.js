dabc2b4437f3c4fffed7200842a634caeac3fd26
Rafael Luz <133050027+rafaeldesp@users.noreply.github.com>
2025-01-04 11:33:56 -0300

    Delete jimmyPage directory

--- jimmyPage/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/*.{html,js}"],
-  theme: {
-    extend:{backgroundImage: {
-      bannerImg: "url('/imgs/bannerImg.jpg')"
-    }, },
-  plugins: [],
-}}
